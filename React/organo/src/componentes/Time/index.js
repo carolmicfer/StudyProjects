b2712107fa41289507f7e96d01fc16c2c879d885
Carolina Ferreira <ninaferreira2409@gmail.com>
2023-08-18 17:54:59 -0300

    add: deletarColaborador()

--- React/organo/src/componentes/Time/index.js
+++ React/organo/src/componentes/Time/index.js
@@ -8,7 +8,10 @@ const Time = (props) => {
     <section className="time" style={css}>
       <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
       <div className="colaboradores">
-      {props.colaboradores.map(colaborador => <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} corPrimaria={props.corPrimaria}/>)}
+      {props.colaboradores.map(colaborador => {
+        return <Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} corPrimaria={props.corPrimaria}
+        aoDeletar={props.aoDeletar}/>
+      })}
       </div>
       
     </section>
