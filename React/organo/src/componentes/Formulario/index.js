5494ca691b2d78a7e4a434822d7d13798c7d3384
Carolina Ferreira <ninaferreira2409@gmail.com>
2023-08-22 12:04:22 -0300

    feat: aula4

--- React/organo/src/componentes/Formulario/index.js
+++ React/organo/src/componentes/Formulario/index.js
@@ -21,9 +21,12 @@ const Formulario = (props) => {
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
 
+    const [nomeTime, setNomeTime] = useState('')
+    const [corTime, setCorTime] = useState('')
+
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        props.aoColaboradorCadastrado({
+        props.aoCadastrar({
             nome,
             cargo,
             imagem,
@@ -68,6 +71,26 @@ const Formulario = (props) => {
                 />
                 <Botao>
                     Criar Card
+                </Botao>
+            </form>
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar um novo time</h2>
+                <CampoTexto 
+                    obrigatorio
+                    label="Nome"
+                    placeholder="Digite o nome do time" 
+                    valor={nomeTime}
+                    aoAlterado={valor => setNomeTime(valor)}
+                />
+                <CampoTexto
+                    obrigatorio
+                    label="Cor" 
+                    placeholder="Digite a cor do time" 
+                    valor={corTime}
+                    aoAlterado={valor => setCorTime(valor)}
+                />
+                <Botao>
+                    Criar um novo time
                 </Botao>
             </form>
         </section>
--- React/organo/src/componentes/Time/index.js
+++ React/organo/src/componentes/Time/index.js
@@ -1,17 +1,32 @@
-import Colaborador from '../Colaborador'
-import './Time.css'
+import hexToRgba from 'hex-to-rgba';
+import Colaborador from "../Colaborador";
+import "./Time.css";
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
-    return (
-
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
-            <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />)}
-            </div>
-        </section>
-
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
+  return (
+    colaboradores.length > 0 && (
+      <section
+        className="time"
+        style={{
+          backgroundImage: "url(/imagens/fundo.png)",
+          backgroundColor: hexToRgba(time.cor, '0.3'),
+        }}
+      >
+        <input value={time.cor} onChange={evento => mudarCor(evento.target.value, time.id)} type="color" className="input-cor" />
+        <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+        <div className="colaboradores">
+          {colaboradores.map((colaborador, indice) => (
+            <Colaborador
+              key={indice}
+              colaborador={colaborador}
+              corDeFundo={time.cor}
+              aoDeletar={aoDeletar}
+            />
+          ))}
+        </div>
+      </section>
     )
-}
+  );
+};
 
-export default Time
+export default Time;
