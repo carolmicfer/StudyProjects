c1e37e707c539fb019147da3de895a1215bc97da
Carolina Ferreira <ninaferreira2409@gmail.com>
2023-08-29 11:27:05 -0300

    add: components

---
+++ React/alura-space/src/componentes/Galeria/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Tags from "../Tags";
+import Cards from "./Cards";
+import styles from "./Galeria.module.scss";
+import fotos from "./fotos.json";
+
+export default function Galeria() {
+  return (
+    <section className={styles.galeria}>
+      <h2>Navegue pela galeria</h2>
+      <Tags />
+      <Cards itens={fotos} styles={styles} />
+    </section>
+  );
+}
