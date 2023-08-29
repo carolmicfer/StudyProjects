838978699a08e72b6a1e06269e0f4d8d457c66d2
Carolina Ferreira <ninaferreira2409@gmail.com>
2023-08-29 11:27:05 -0300

    add: components

--- React/alura-space/src/paginas/PaginaInicial/index.jsx
+++ React/alura-space/src/paginas/PaginaInicial/index.jsx
@@ -1,11 +1,27 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Galeria from "../../componentes/Galeria";
 import Menu from "../../componentes/Menu";
+import Populares from "../../componentes/Populares";
+import banner from "./banner.png";
+import styles from "./PaginaInicial.module.scss";
 
 export default function PaginaInicial() {
   return (
     <>
       <Cabecalho />
-      <Menu />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra  vista do espaço" />
+          </div>
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
+      </main>
     </>
   );
 }
